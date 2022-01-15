7b334488617b352fd41308a13bdbf2f55b4d29ac
Tymoteusz Boba <Tymoteusz.Boba@sabre.com>
2022-01-15 15:22:42 +0100

    Add paginated endpoints for user controller

--- src/main/java/pl/wilczadruzyna/biteshare/controller/UserController.java
+++ src/main/java/pl/wilczadruzyna/biteshare/controller/UserController.java
@@ -1,11 +1,14 @@
 package pl.wilczadruzyna.biteshare.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,19 +34,32 @@ public class UserController {
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(value = "/getAllUsers")
-    public ResponseEntity<List<User>> getAllUsers() {
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok(repository.findAll());
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @PostMapping(value = "/add/{user}")
-    public ResponseEntity<User> addUser(@PathVariable("user") User user) {
+    @GetMapping(value = "/custom")
+    public ResponseEntity<Page<User>> getUsersPaginated(Pageable pageable) {
+        return ResponseEntity.ok(repository.findAll(pageable));
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping(value = "/add")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
         return ResponseEntity.ok(repository.save(user));
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @DeleteMapping(value = "/deleteAll")
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> deleteAll(@PathVariable("id") long id) {
+        repository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping(value = "/all")
     public ResponseEntity<?> deleteAll() {
         repository.deleteAll();
         return ResponseEntity.ok().build();
