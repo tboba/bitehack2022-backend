937eb09e8a5289af1e444e3516303b6ae9d365e1
Tymoteusz Boba <Tymoteusz.Boba@sabre.com>
2022-01-15 22:49:16 +0100

    Add better values to mocks

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,9 +10,15 @@ INSERT INTO post_categories (name) VALUES ('pieczywo'),
                                      ('dania na ciepło'),
                                      ('dania do odgrzania');
 
-INSERT INTO posts (title, description, creation_date, expiry_date, image_url, post_category_id, author_id) VALUES
-    ('TestTitle1', 'TestDesc1', '2022-01-15', '2022-01-16', 'TestUrl1', 1, 1),
-    ('TestTitle2', 'TestDesc2', '2022-01-15', '2022-01-16', 'TestUrl2', 2, 2),
-    ('TestTitle3', 'TestDesc3', '2022-01-15', '2022-01-16', 'TestUrl3', 3, 3),
-    ('TestTitle4', 'TestDesc4', '2022-01-15', '2022-01-16', 'TestUrl4', 4, 4),
-    ('TestTitle5', 'TestDesc5', '2022-01-15', '2022-01-16', 'TestUrl5', 1, 4);
+INSERT INTO location (latitude, longitude, city) VALUES (50.069490, 19.914944, 'Kraków'),
+                                                        (50.072996, 19.911225, 'Kraków'),
+                                                        (50.072996, 19.911225, 'Kraków'),
+                                                        (50.064857, 19.925906, 'Kraków'),
+                                                        (50.071209, 19.926891, 'Kraków');
+
+INSERT INTO posts (title, description, creation_date, expiry_date, image_url, post_category_id, author_id, destination_id) VALUES
+    ('Chleb pszenny', 'Kategoria: pieczywo | waga: 500g', '2022-01-15', '2022-01-17', 'https://i.imgur.com/bBApToy.jpg', 1, 1, 1),
+    ('Serek homogenizowany Danio', 'Kategoria: nabiał | waga: 150g', '2022-01-15', '2022-01-24', 'https://i.imgur.com/ceTywVz.png', 2, 2, 2),
+    ('Papryka czerwona', 'Kategoria: warzywa | waga: 200g', '2022-01-15', '2022-01-18', 'https://i.imgur.com/uxleAuS.jpg', 3, 3, 3),
+    ('Zupka chińska Vifon', 'Kategoria: fast-food | waga: 80g', '2022-01-15', '2023-01-28', 'https://i.imgur.com/pMkoqNG.jpg', 4, 4, 4),
+    ('Pepsi 2L', 'Kategoria: napoje gazowane | waga: 800g', '2022-01-15', '2022-08-21', 'https://i.imgur.com/ORKRGIi.jpg', 1, 4, 5);
