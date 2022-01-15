d0cab6ccae93b7a00f79858e82012155986df2cb
Tymoteusz Boba <Tymoteusz.Boba@sabre.com>
2022-01-15 13:47:54 +0100

    Add bootstrap for launching application

---
+++ src/main/java/pl/wilczadruzyna/biteshare/BiteshareApplication.java
@@ -0,0 +1,13 @@
+package pl.wilczadruzyna.biteshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiteshareApplication.class, args);
+    }
+
+}
