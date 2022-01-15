ee0601e5fffac4ca83656d4c68666848ee8362d2
Tymoteusz Boba <Tymoteusz.Boba@sabre.com>
2022-01-15 22:15:32 +0100

    Fix adding and patching posts

--- src/main/java/pl/wilczadruzyna/biteshare/controller/PostController.java
+++ src/main/java/pl/wilczadruzyna/biteshare/controller/PostController.java
@@ -5,6 +5,7 @@ import com.google.maps.errors.ApiException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,6 +25,7 @@ import pl.wilczadruzyna.biteshare.repository.UserRepository;
 import pl.wilczadruzyna.biteshare.service.LocationService;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -68,6 +70,13 @@ public class PostController {
 
         if (post == null) {
             post = new Post();
+            post.setTitle(payload.get("title").asText());
+            post.setDescription(payload.get("description").asText());
+            post.setCreationDate(LocalDate.parse(payload.get("creationDate").asText()));
+            post.setExpiryDate(LocalDate.parse(payload.get("expiryDate").asText()));
+            post.setImageUrl(payload.get("imageUrl").asText());
+        } else {
+            return ResponseEntity.badRequest().build();
         }
 
         if (payload.get("postCategory") != null && payload.get("postCategory").get("name") != null) {
@@ -107,7 +116,52 @@ public class PostController {
     }
 
     @PatchMapping
-    public ResponseEntity<Post> patch(Post post) {
+    public ResponseEntity<Post> patch(@RequestBody JsonNode payload) throws IOException, InterruptedException, ApiException {
+        Post post = postRepository.findPostByTitle(payload.get("title").asText());
+
+        if (post != null) {
+            post.setTitle(payload.get("title").asText());
+            post.setDescription(payload.get("description").asText());
+            post.setCreationDate(LocalDate.parse(payload.get("creationDate").asText()));
+            post.setExpiryDate(LocalDate.parse(payload.get("expiryDate").asText()));
+            post.setImageUrl(payload.get("imageUrl").asText());
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (payload.get("postCategory") != null && payload.get("postCategory").get("name") != null) {
+            PostCategory postCategory = postCategoryRepository.findPostCategoryByName(payload.get("postCategory").get("name").asText());
+            if (postCategory != null) {
+                post.setPostCategory(postCategory);
+            }
+        }
+
+        if (payload.get("author") != null && payload.get("author").get("name") != null) {
+            User author = userRepository.findUserByName(payload.get("author").get("name").asText());
+            if (author != null) {
+                post.setAuthor(author);
+            }
+        }
+
+        if (payload.get("location") != null && payload.get("location").get("latitude") != null
+                && payload.get("location").get("longitude") != null) {
+            Location location = locationRepository.findLocationByLatitudeAndLongitude(payload.get("location").get("latitude").asDouble(),
+                    payload.get("location").get("longitude").asDouble());
+
+            if (location == null) {
+                location = new Location();
+                location.setLatitude(payload.get("location").get("latitude").asDouble());
+                location.setLongitude(payload.get("location").get("longitude").asDouble());
+                locationRepository.save(location);
+            }
+
+            post.setLocation(location);
+        }
+
+        Location location = post.getLocation();
+        String decodedCity = locationService.findExactCityFrom(location.getLatitude(), location.getLongitude());
+        location.setCity(decodedCity);
+
         return ResponseEntity.ok(postRepository.save(post));
     }
 
